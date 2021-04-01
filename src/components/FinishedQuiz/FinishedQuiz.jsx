6a27ce662a645d389159180f4f37b52d08599cd3
Dmitry Verteyko <verteyko1990@gmail.com>
2021-04-01 23:51:27 +0300

    added api axios

--- src/components/FinishedQuiz/FinishedQuiz.jsx
+++ src/components/FinishedQuiz/FinishedQuiz.jsx
@@ -5,6 +5,7 @@ import classes from './finishedQuiz.module.scss';
 import PropTypes from 'prop-types';
 
 const FinishedQuiz = props => {
+    console.log('props', props)
     const successCount = Object.keys(props.results).reduce((total, key) => {
         if (props.results[key] === 'success') {
             total++
